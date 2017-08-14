99f7f96a9d930c8515d3b25459721287fee7c824
Pablo Roberto Carrera Estrada <pablocarrera3@gmail.com>
2017-08-14 11:49:16 -0500

    generate de main modules to work on

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { HistoryComponent } from './history/history.component';
+import { LocationComponent } from './location/location.component';
+import { ContactComponent } from './contact/contact.component';
+import { AwardsComponent } from './awards/awards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    HistoryComponent,
+    LocationComponent,
+    ContactComponent,
+    AwardsComponent
   ],
   imports: [
     BrowserModule
