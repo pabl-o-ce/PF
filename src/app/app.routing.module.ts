d637f2067df1716da38c0efe1bbe26538934fb88
Pablo Roberto Carrera Estrada <pablocarrera3@gmail.com>
2017-08-14 12:06:25 -0500

    adding route module

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ContactComponent } from './contact/contact.component';
+import { HistoryComponent } from './history/history.component';
+import { LocationComponent } from './location/location.component';
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot([
+            { path: '', component: MenuComponent },
+            { path: 'contact', component: ContactComponent },
+            { path: 'history', component: HistoryComponent },
+            { path: 'location', component: LocationComponent },
+            { path: 'menu', component: MenuComponent },
+            // { path: '**', redirectTo: '' }
+        ]),
+    ],
+    exports: [
+        RouterModule,
+    ],
+    providers: [],
+})
+export class AppRoutingModule { }
