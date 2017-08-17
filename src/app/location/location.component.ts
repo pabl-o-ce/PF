e542a7660cd15e5ab6c162f3c67f0e5f7bb91d2d
Pablo Roberto Carrera Estrada <pablocarrera3@gmail.com>
2017-08-17 12:31:39 -0500

    adding location component

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -16,10 +16,230 @@ export class LocationComponent implements OnInit {
 
   initMap() {
     const uluru = {lat: -0.201561, lng: -78.4310699};
+    const styledMapType = new google.maps.StyledMapType([
+  {
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#ebe3cd'
+      }
+    ]
+  },
+  {
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#523735'
+      }
+    ]
+  },
+  {
+    'elementType': 'labels.text.stroke',
+    'stylers': [
+      {
+        'color': '#f5f1e6'
+      }
+    ]
+  },
+  {
+    'featureType': 'administrative',
+    'elementType': 'geometry.stroke',
+    'stylers': [
+      {
+        'color': '#c9b2a6'
+      }
+    ]
+  },
+  {
+    'featureType': 'administrative.land_parcel',
+    'elementType': 'geometry.stroke',
+    'stylers': [
+      {
+        'color': '#dcd2be'
+      }
+    ]
+  },
+  {
+    'featureType': 'administrative.land_parcel',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#ae9e90'
+      }
+    ]
+  },
+  {
+    'featureType': 'landscape.natural',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#dfd2ae'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#dfd2ae'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#93817c'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi.park',
+    'elementType': 'geometry.fill',
+    'stylers': [
+      {
+        'color': '#a5b076'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi.park',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#447530'
+      }
+    ]
+  },
+  {
+    'featureType': 'road',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#f5f1e6'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.arterial',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#fdfcf8'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.highway',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#f8c967'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.highway',
+    'elementType': 'geometry.stroke',
+    'stylers': [
+      {
+        'color': '#e9bc62'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.highway.controlled_access',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#e98d58'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.highway.controlled_access',
+    'elementType': 'geometry.stroke',
+    'stylers': [
+      {
+        'color': '#db8555'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.local',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#806b63'
+      }
+    ]
+  },
+  {
+    'featureType': 'transit.line',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#dfd2ae'
+      }
+    ]
+  },
+  {
+    'featureType': 'transit.line',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#8f7d77'
+      }
+    ]
+  },
+  {
+    'featureType': 'transit.line',
+    'elementType': 'labels.text.stroke',
+    'stylers': [
+      {
+        'color': '#ebe3cd'
+      }
+    ]
+  },
+  {
+    'featureType': 'transit.station',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#dfd2ae'
+      }
+    ]
+  },
+  {
+    'featureType': 'water',
+    'elementType': 'geometry.fill',
+    'stylers': [
+      {
+        'color': '#b9d3c2'
+      }
+    ]
+  },
+  {
+    'featureType': 'water',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#92998d'
+      }
+    ]
+  }
+]);
     const map = new google.maps.Map(document.getElementById('map'), {
       zoom: 18,
-      center: uluru
+      center: uluru,
+      mapTypeControlOptions: {
+        mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain', 'styled_map']
+      }
     });
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
     const marker = new google.maps.Marker({
       animation: google.maps.Animation.DROP,
       position: uluru,
@@ -27,9 +247,7 @@ export class LocationComponent implements OnInit {
       icon: './mapicon.png',
       title: 'El Palacio de la Fritada'
     });
-    const contentString = '<h3 id="firstHeading" class="firstHeading">El Palacio de la Fritada</h3>' +
-                                          '<p>Encuentranos en Salinas y Maria Angelica Hidrobo, cerca del Parque de Cumbaya.</p>' +
-                                          '<p>Te Esperamos!</p>';
+    const contentString = '<h3>El Palacio de la Fritada</h3><p>Encuentranos en Salinas y Maria Angelica Hidrobo, cerca del Parque de Cumbaya.</p><p>Te Esperamos!</p>';
     const infowindow = new google.maps.InfoWindow({
       content: contentString,
       maxWidth: 200
